39cc94aa3d8665d2baac1a8ee85e8d2b76a0c0a2
Martyn Clinton <ausipome@gmail.com>
2026-05-10 19:38:06 +0100

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,31 @@
 import "./globals.css";
 import { AuthProvider } from "@/components/auth/AuthProvider";
 import CookieConsent from "@/components/CookieConsent";
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "QuotePadPro",
-  description: "Create and send professional quotes",
+  description: "Create and send professional quotes in seconds with QuotePadPro. Our intuitive platform streamlines the quoting process, allowing you to generate accurate and visually appealing quotes effortlessly. Whether you're a freelancer, small business owner, or enterprise, QuotePadPro is your go-to solution for efficient and professional quoting.",
+
+  icons: {
+    icon: [
+      {
+        url: "/favicon/favicon-96x96.png",
+        sizes: "96x96",
+        type: "image/png",
+      },
+      {
+        url: "/favicon/favicon.svg",
+        type: "image/svg+xml",
+      },
+      {
+        url: "/favicon/favicon.ico",
+      },
+    ],
+    apple: "/favicon/apple-touch-icon.png",
+  },
+
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({
